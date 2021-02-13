8397465d354a7549703d24d58dab5ff212d0254c
Elijah Kurganskii <ilya.kurganskii@gmail.com>
2021-02-13 21:09:21 +0400

    Implement GitHub workflow (#4)
    * feat(github): add github workflow
    
    * feat(github): add prisma generate
    
    * feat(github): update workflow
    
    * feat(github): update workflow
    
    * feat(github): add upload code coverage
    
    * feat(github): fix issues
    
    * feat(github): add build docker image
    
    * feat(github): add build docker image
    
    * feat(github): add build docker image
    
    Co-authored-by: Ilia Kurganskiy <kurganskiy@mercurydevelopment.com>

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
